ddfce313ba440ce0316e980b6b23ace49e24fa40
mward629 <mward@nightingale.org>
2026-02-25 09:59:40 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 import random
+import os
 
 app = Flask(__name__)
 
@@ -26,5 +27,7 @@ def generate():
 
     return jsonify({"offspring": offspring})
 
+import os
+
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
